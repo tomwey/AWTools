3ef2abfa012caa3b874dcbf6d49898188ebea984
tomwey <tomwey@163.com>
2016-05-09 12:13:29 +0800

    Initial commit

---
+++ Example/AWTools/AWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AWAppDelegate.h
+//  AWTools
+//
+//  Created by tomwey on 05/09/2016.
+//  Copyright (c) 2016 tomwey. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
